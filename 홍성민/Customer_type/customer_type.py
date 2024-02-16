324ea6980a1bd3502d29e18446fea01c864f843e
홍성민 <landsky1234@naver.com>
2024-02-16 14:04:27 +0900

    Update customer_type.py

--- 홍성민/Customer_type/customer_type.py
+++ 홍성민/Customer_type/customer_type.py
@@ -1,6 +1,6 @@
 def cus_type_typo(df) :
-
-  #  == 오타 먼저 전처리 ==
+  
+  #  == 오타 먼저 전처리 == 
   replace_values_typo = {
       'End Customer' : 'End-Customer',
       'Other' : 'Others',
@@ -12,8 +12,8 @@ def cus_type_typo(df) :
       'Software / Solution Provider' : 'Software/Solution Provider',
 
   }
-
-  # == 대체하기 ==
+  
+  # == 대체하기 == 
   replace_values =  {
       'Solution Eco-Partner' : 'Service Partner',
       'Engineer' : 'Specifier/ Influencer',
@@ -21,9 +21,9 @@ def cus_type_typo(df) :
       'Technician' : 'Specifier/ Influencer',
       'Consultant' : 'Service Partner',
       'Developer' : 'Specifier/ Influencer',
-      'Homeowner' : 'End-Customer',
+      'Homeowner' : 'End-Customer', 
       'Software/Solution Provider' : 'Service Partner', # 종합적인 solution을 제공한다고 생각하여 SP
-      'Software / Solution Provider' : 'Service Partner', #
+      'Software / Solution Provider' : 'Service Partner', # 
       'Manager / Director' : 'Specifier/ Influencer',
       'Installer' : 'Channel Partner', # 설치업자는 현장 설치를 전문적으로 수행한다고 생각함
       'Installer/Contractor' : 'Channel Partner',
@@ -39,6 +39,11 @@ def cus_type_typo(df) :
   }
   df['customer_type'] = df['customer_type'].replace(replace_values_typo)
   df['customer_type'] = df['customer_type'].replace(replace_values)
+
+  # 결측치 처리
+  df.fillna('Unknown', inplace = True)
+
+  
 
   return df
 
